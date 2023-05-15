a71a3acf363772eda5522b94f4a0c8109e732040
David Calaiò <davidcjr11@gmail.com>
2023-05-15 23:55:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="d-flex justify-content-center mt-5 container">
+
+        <form action="index-post.php" method="post" class="w-100">
+            Paragrafo: <input type="text" name="paragrafo" class="d-block mt-1 mb-3 w-100">
+            Parola da censurare: <input type="text" name="censura" class="d-block mt-1 mb-3">
+            <input type="submit">
+        </form>
+
+    </div>
+</body>
+</html>
